fe0d5b473a956e31d40815d53c9320494a7a1fa4
User <user@user-VirtualBox.(none)>
2013-04-26 09:42:30 -0300

    repo inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrin::Application.config.secret_token = 'd4f057f82b8c89127e25fb57db3f4d387e6273ed19eec50c69283a3c934c5367bdb34a624d42ab9427179364c5273ff2f00ed67560afee75a9596b6053dac590'
