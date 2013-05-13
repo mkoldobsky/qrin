abb6727c4ad719c8625fc4b815338299ea6cba1c
Mariano <mkoldobsky@gmail.com>
2013-05-13 12:41:57 -0300

    save awkin falta latlong

--- app/controllers/awkin_controller.rb
+++ app/controllers/awkin_controller.rb
@@ -3,7 +3,19 @@ class AwkinController < ApplicationController
 def index
 	@last = Awkin.last
 	@total = @last.total + 1
-	 respond_to do |format|
+	@awkin = Awkin.create
+	@awkin.date = Date.today
+	@awkin.total = @total
+	@lat_lng = cookies[:lat_lng]
+	if (!@lat_lng.nil?)
+		@lat_lng.split("|")
+	@awkin.lat = @lat_lng[0]
+	@awkin.long = @lat_long[1]
+end
+	@awkin.save
+
+
+	respond_to do |format|
       format.html 
       format.json { render json: @total.to_s }
     end
