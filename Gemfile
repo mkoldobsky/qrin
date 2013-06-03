ac6fddd13d58312da428a174ec4238b5dc62bc31
Mariano <mkoldobsky@gmail.com>
2013-06-03 15:24:57 -0300

    test deploy

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '0.12.2'
+  #gem 'pg', '0.12.2'
 end
 
 gem 'jquery-rails', '2.0.2'
--- app/assets/javascripts/geo.js
+++ app/assets/javascripts/geo.js
@@ -1,17 +1,17 @@
-function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(function(position){    
-  	$.ajax({
-           url: "/awkin/setGeoLocation",
-           data: 'lat=' + position.coords.latitude  + '&long=' + position.coords.longitude
-       });
-  });
+// function getGeoLocation() {
+//   navigator.geolocation.getCurrentPosition(function(position){    
+//   	$.ajax({
+//            url: "/awkin/setGeoLocation",
+//            data: 'lat=' + position.coords.latitude  + '&long=' + position.coords.longitude
+//        });
+//   });
 
-}
+// }
 
-function setGeoLocation(position) {
-  $.ajax({
-           url: "/awkin/setGeoLocation",
-           data: 'lat=' + position.coords.latitude  + '&long=' + position.coords.longitude + '&total=' + $.("#total").val())
-}
+// function setGeoLocation(position) {
+//   $.ajax({
+//            url: "/awkin/setGeoLocation",
+//            data: 'lat=' + position.coords.latitude  + '&long=' + position.coords.longitude + '&total=' + $.("#total").val())
+// }
 
 
